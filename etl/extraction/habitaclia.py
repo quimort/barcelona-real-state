38f06b7485df71a73d0e32471b977124e8933a49
quim <quimort97@gmail.com>
2024-07-02 19:07:31 +0200

    start to play with selenium

---
+++ etl/extraction/habitaclia.py
@@ -0,0 +1,30 @@
+#import modules
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+def get_chrome_driver():
+    driver = webdriver.Chrome()
+    return driver
+
+def main():
+
+    #set chrome web driver
+    driver = get_chrome_driver()
+    #get web page
+    driver.get("https://www.habitaclia.com/viviendas-barcelona.htm")
+
+    a = driver.find_element(By.NAME,'h3')
+
+    #print page source
+    print(a)
+
+    #close web driver
+    driver.close()
+
+
+
+if __name__ == "__main__":
+    main()
